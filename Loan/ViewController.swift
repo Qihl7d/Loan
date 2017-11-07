aae452b441f65e2fad2ab6871dede6d846514c71
Andylau_code <447696672@qq.com>
2017-11-07 11:36:02 +0800

    Initial Commit

---
+++ Loan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Loan
+//
+//  Created by lau Andy on 2017/11/7.
+//  Copyright © 2017年 lau Andy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
